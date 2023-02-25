ea966ed90df917696fd6998d3dcbc14d4c6cdf9a
sam-barbeau <sbarb3@illinois.edu>
2023-02-24 22:14:11 -0600

    title change

---
+++ includes/variables.js
@@ -0,0 +1,7 @@
+const buttonA = document.querySelector('#button_A');
+const headingA = document.querySelector('#heading_A');
+
+buttonA.onclick = () => {
+  const name = prompt('What is your name?');
+  headingA.textContent = `Welcome ${name}`;
+}
