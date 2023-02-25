fe47bf12295d52fa72c1f950174864b8567731dc
sam-barbeau <sbarb3@illinois.edu>
2023-02-25 00:05:00 -0600

    deleted includes folder and fixed links

--- includes/variables.js
+++
@@ -1,7 +0,0 @@
-const buttonA = document.querySelector('#button_A');
-const headingA = document.querySelector('#heading_A');
-
-buttonA.onclick = () => {
-  const name = prompt('What is your name?');
-  headingA.textContent = `Welcome ${name}`;
-}
